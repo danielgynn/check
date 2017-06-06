5f618ad791c2e1f3ae6762b38568ca9668c727d9
danielgynn <danielgynn@hotmail.co.uk>
2017-06-06 13:17:42 +0100

    Allow todo to expand for notes

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -7,11 +7,13 @@ class Todo extends Component {
       text: this.props.text,
       tag: this.props.tag,
       deadline: this.props.deadline,
-      notes: this.props.notes
+      notes: this.props.notes,
+      notesDisplay: false
     };
 
     this.handleCheckClick = this.handleCheckClick.bind(this);
     this.deleteTodo = this.deleteTodo.bind(this);
+    this.toggleNotesDisplay = this.toggleNotesDisplay.bind(this);
   }
 
   handleCheckClick(e) {
@@ -31,6 +33,12 @@ class Todo extends Component {
     this.props.onTodoDelete(id);
   }
 
+  toggleNotesDisplay(e) {
+    e.preventDefault();
+    let display = (this.state.notesDisplay) ? false : true;
+    this.setState({ notesDisplay: display });
+  }
+
   render() {
     return (
       <div>
@@ -39,13 +47,16 @@ class Todo extends Component {
             <input type='checkbox' id={ this.state.text } onChange={ this.handleCheckClick } checked={ this.props.complete } defaultChecked={ this.state.complete } />
             <label htmlFor={ this.state.text } />
           </form>
-          <div className='check-details'>
-            <p>{ this.state.text }</p>
-            <div>
-              <button onClick={ this.deleteTodo }>Delete</button>
+          <div className='todo-details' onClick={ this.toggleNotesDisplay }>
+            <div className='check-details'>
+              <p>{ this.state.text }</p>
+              <div>
+                <button onClick={ this.deleteTodo }>Delete</button>
+              </div>
             </div>
           </div>
         </div>
+        { (this.state.notesDisplay) ? (<div><h3>Notes</h3></div>) : null }
         <hr />
       </div>
     );
